4f5315500b11b865eb101bea44be1b810c53f6d2
DucVAHE140108 <DucVAHE140108@fpt.edu.vn>
2021-09-16 16:55:50 +0700

    update model

---
+++ Test1/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Test1.Models
+{
+    public class Category
+    {
+        [Required, MaxLength(50)]
+        public string CategoryName { get; set; }
+        [Required]
+        public int Order { get; set; }
+        public string Notes { get; set; }
+
+        public ICollection<Flower> Flowers { get; set; }
+    }
+}
---
+++ Test1/Models/Flower.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Test1.Models
+{
+    public class Flower
+    {
+        [Required, MaxLength(255)]
+        public string FlowerName { get; set; }
+        public string Description { get; set; }
+        public string Color { get; set; }
+        public string Image { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public DateTime StoreDate{ get; set; }
+        [Required]
+        public int StoreInventory { get; set; }
+
+        public Category Categories{ get; set; }
+    }
+}
